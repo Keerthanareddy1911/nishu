6e84071c809350725519f47324331f27af73839b
keerthana <tharunreddy@Tharuns-MacBook-Air.local>
2024-12-23 22:30:54 +0530

    final

--- calculator.c
+++ calculator.c
@@ -2,7 +2,10 @@
 int mul(int a,int b){
 return a*b;
 }
+int add(int a,int b){
+    return a+b;
+}
 int main(){
-printf("%d,add(3,4));
-printf(%d",mul(8,9));
+printf("%d",add(3,4));
+printf("%d",mul(8,9));
 }
